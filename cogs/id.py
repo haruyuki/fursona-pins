d9052d56db2ce7dac1e1126de106823a916aa340
haruyuki <jumpy12359@gmail.com>
2019-05-27 00:10:36 +1000

    Create .id command

---
+++ cogs/id.py
@@ -0,0 +1,110 @@
+import json
+
+import aiohttp
+import discord
+from discord.ext import commands
+
+from constants import Constants, FursonaPins
+
+exceptions = {147, 200, 229, 318}
+
+
+class Identifier(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        self.spreadsheet = Constants.google_sheets_api.open_by_key('1bYyZmN_Bm5NGHpRL-ox-5sn5kqsTDLNiLuyrbsEcEbY')  # Link to conversion spreadsheet
+        self.worksheet = self.spreadsheet.sheet1
+
+    @commands.command(aliases=['id'])
+    @commands.guild_only()
+    async def identifier(self, ctx, identifier: int):
+        if identifier in exceptions:
+            try:
+                identifier_cell = self.worksheet.find(str(identifier), matchEntireCell=True)[0]
+            except IndexError:
+                await ctx.send('An unknown error has occured, please contact the bot creator.')
+                return
+
+            pin_name = identifier_cell.neighbour((0, 1)).value
+            pin_owner = identifier_cell.neighbour((0, 2)).value
+
+            title = f'{pin_name} (#{f"{identifier:03}"})'
+            description = f'Owned by: {pin_owner}'
+            file = discord.File(fp=f'exceptions/{identifier}.png', filename=f'{identifier}.png')
+            embed = discord.Embed(title=title, description=description, colour=Constants.embed_colour)
+            embed.set_image(url=f'attachment://{identifier}.png')
+            embed.set_thumbnail(url=FursonaPins.banner_logo)
+            await ctx.send(file=file, embed=embed)
+            return
+
+        if identifier <= 20:  # 1
+            api_call = FursonaPins.fursona_pins_api + '1'
+        elif identifier <= 40:  # 2
+            api_call = FursonaPins.fursona_pins_api + '2'
+        elif identifier <= 60:  # 3
+            api_call = FursonaPins.fursona_pins_api + '3'
+        elif identifier <= 80:  # 4
+            api_call = FursonaPins.fursona_pins_api + '4'
+        elif identifier <= 100:  # 5
+            api_call = FursonaPins.fursona_pins_api + '5'
+        elif identifier <= 120:  # 6
+            api_call = FursonaPins.fursona_pins_api + '6'
+        elif identifier <= 140:  # 7
+            api_call = FursonaPins.fursona_pins_api + '7'
+        elif identifier <= 161:  # 8
+            api_call = FursonaPins.fursona_pins_api + '8'
+        elif identifier <= 181:  # 9
+            api_call = FursonaPins.fursona_pins_api + '9'
+        elif identifier <= 202:  # 10
+            api_call = FursonaPins.fursona_pins_api + '10'
+        elif identifier <= 222:  # 11
+            api_call = FursonaPins.fursona_pins_api + '11'
+        elif identifier <= 243:  # 12
+            api_call = FursonaPins.fursona_pins_api + '12'
+        elif identifier <= 263:  # 13
+            api_call = FursonaPins.fursona_pins_api + '13'
+        elif identifier <= 283:  # 14
+            api_call = FursonaPins.fursona_pins_api + '14'
+        elif identifier <= 303:  # 15
+            api_call = FursonaPins.fursona_pins_api + '15'
+        elif identifier <= 324:  # 16
+            api_call = FursonaPins.fursona_pins_api + '16'
+        elif identifier <= 344:  # 17
+            api_call = FursonaPins.fursona_pins_api + '17'
+        elif identifier <= 364:  # 18
+            api_call = FursonaPins.fursona_pins_api + '18'
+        elif identifier <= 402:  # 19
+            api_call = FursonaPins.fursona_pins_api + '19'
+        else:  # 20+
+            api_call = FursonaPins.fursona_pins_api + '20'
+
+        async with aiohttp.ClientSession() as session:  # Create an AIOHTTP session
+            async with session.get(api_call, headers=Constants.http_headers) as response:
+                if response.status == 200:  # If received response is OK
+                    connection = await response.text()  # Get text HTML of site
+        data = json.loads(connection)
+        data = data['data']
+        for pin in data:
+            if pin['pseudo'] == identifier:
+                try:
+                    identifier_cell = self.worksheet.find(str(identifier), matchEntireCell=True)[0]
+                except IndexError:
+                    await ctx.send('An unknown error has occured, please contact the bot creator.')
+                    return
+
+                pin_name = identifier_cell.neighbour((0, 1)).value
+                pin_owner = identifier_cell.neighbour((0, 2)).value
+
+                title = f'{pin_name} (#{f"{identifier:03}"})'
+                description = f'Owned by: {pin_owner}'
+                pin_image = 'https://www.fursonapins.com/' + pin['image'].replace('\\', '')
+                embed = discord.Embed(title=title, description=description, colour=Constants.embed_colour)
+                embed.set_image(url=pin_image)
+                embed.set_thumbnail(url=FursonaPins.banner_logo)
+                await ctx.send(embed=embed)
+                return
+        await ctx.send("There's no pin with that ID found :frowning:")
+
+
+def setup(bot):
+    bot.add_cog(Identifier(bot))
---
+++ exceptions/README.md
@@ -0,0 +1,6 @@
+All images in this folder belong to their respective owners.
+
+* **Pin 147 (Aquatifur)** - Bubbles
+* **Pin 200 (Frostbite)** - Cy Mendoza
+* **Pin 229 (Sorbet Jungle** - Sorbet Jungle Shop
+* **Pin 318 (Howl and Fig)** - Bubbles
