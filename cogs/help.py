45e21e0ee498b7de9b58487284dd161a17efca6b
haruyuki <jumpy12359@gmail.com>
2019-05-27 00:13:53 +1000

    Capitalisation

--- cogs/help.py
+++ cogs/help.py
@@ -13,8 +13,8 @@ class Help(commands.Cog):
     async def help(self, ctx):
         embed = discord.Embed(title='Fursona Pins', description='Available commands:', colour=Constants.embed_colour)
         embed.add_field(name='**.about**', value='Display information about the bot', inline=False)
-        # embed.add_field(name='**.ch <name>**', value='Find a fursona pin based on the character name', inline=False)
-        embed.add_field(name='**.id <Pin ID>**', value="Find a fursona pin based on it's ID", inline=False)
+        # embed.add_field(name='**.ch <name>**', value='Find a Fursona Pin based on the character name', inline=False)
+        embed.add_field(name='**.id <Pin ID>**', value="Find a Fursona Pin based on it's ID", inline=False)
         embed.add_field(name='**.links**', value="Display links to various Fursona Pins' sites")
         await ctx.send(embed=embed)
 
